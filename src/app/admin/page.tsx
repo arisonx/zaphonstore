a92f18d0502ed49a801e8dc9677904c8a15cbf18
arisonreis <arisontechp@gmail.com>
2024-08-06 00:45:28 -0300

    admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,17 @@
-import { Actions } from "./components/actions";
+import { GetAllProducts } from "../actions/products/getAllProduct";
+import { AdminDialogs } from "./components/dialogs";
 import { Header } from "./components/header";
-export default function Admin() {
-  return (
-    <>
-      <Header />
-      <Actions/>
-    </>
-  );
+import { getCategory } from "@/app/actions/products/getcategory";
+
+export default async function Admin() {
+ const categories = await getCategory();
+
+ const products = await GetAllProducts();
+
+ return (
+  <main className='h-screen w-screen'>
+   <Header />
+   <AdminDialogs categories={categories} products={products} />
+  </main>
+ );
 }
