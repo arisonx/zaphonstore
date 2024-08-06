e4f6d851d107e23df269037865293ea30c710a44
arisonreis <arisontechp@gmail.com>
2024-08-06 00:48:38 -0300

    header component

--- src/app/admin/components/header/index.tsx
+++ src/app/admin/components/header/index.tsx
@@ -2,24 +2,24 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 
 export function Header() {
-  return (
-    <header className="w-full h-[10%] p-4 flex items-center justify-center">
-      <div className="flex w-full items-center  justify-between">
-        <div className="flex items-center gap-4">
-          <Button>Voltar</Button>
-          <Button>Loja</Button>
-        </div>
+ return (
+  <header className='flex h-[10%] w-full items-center justify-center p-4'>
+   <div className='flex w-full items-center justify-between'>
+    <div className='flex items-center gap-2'>
+     <Button className='border-[1px] border-white'>Voltar</Button>
+     <Button className='border-[1px] border-white'>Loja</Button>
+    </div>
 
-        <div className="flex items-center gap-3">
-          <p className="text-white">vo1d</p>
-          <div>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+    <div className='flex items-center gap-3'>
+     <p className='text-white'>vo1d</p>
+     <div>
+      <Avatar>
+       <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+       <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+     </div>
+    </div>
+   </div>
+  </header>
+ );
 }
