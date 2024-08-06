abf3c689ceab17cf4520b6bc857d67ba6a7dfc3b
arisonreis <arisontechp@gmail.com>
2024-08-06 00:45:41 -0300

    dialogs

---
+++ src/app/admin/components/dialogs/index.tsx
@@ -0,0 +1,23 @@
+import { RemoveProductDialog } from "./manageProducts";
+import { LogsViewDialog } from "./logsview";
+import { CategoryType } from "@/@types/category";
+import { ProductType } from "@/@types/products";
+import { ManageCategoriesDialog } from "./manageCategories";
+
+interface AdminDialogsData {
+ categories: CategoryType[] | undefined | null;
+ products: ProductType[] | undefined | null;
+}
+export function AdminDialogs({ categories, products }: AdminDialogsData) {
+ return (
+  <div className='flex h-[90%] w-full flex-col items-center gap-4'>
+   <h2 className='pb-[2rem] pt-[5rem] text-xl text-white'>Zaphon Store Admin</h2>
+   <div className='flex h-[65%] w-full flex-col items-center justify-center gap-2 rounded-md'>
+   
+    <RemoveProductDialog categories={categories} products={products as ProductType[]} />
+    <ManageCategoriesDialog categories={categories} products={products as ProductType[]} />
+    <LogsViewDialog />
+   </div>
+  </div>
+ );
+}
