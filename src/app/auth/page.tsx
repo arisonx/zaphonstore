60e660369312411a2a0399c0120c7a3ae623dc0b
arisonx <arisontechp@gmail.com>
2024-08-06 01:00:04 -0300

    admin auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,51 +3,30 @@ import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 export default function Auth() {
-  return (
-    <div className="h-full w-full flex justify-center items-center flex-col gap-12">
-      <h2 className="text-white font-medium text-2xl">Faça Login</h2>
-      <div className="flex items-center gap-8 border-[1px] border-gray-950 rounded-2xl p-4">
-        <button className="text-white flex gap-2 items-center">
-          <Image
-            src="/discord.svg"
-            width={50}
-            height={50}
-            alt="Uma imagem simbolizando um robô"
-          />
-        </button>
-        <button className="text-white flex gap-2 items-center">
-          <Image
-            src="/google.svg"
-            width={40}
-            height={40}
-            alt="Uma imagem simbolizando um robô"
-          />
-        </button>
-        <button className="text-white flex gap-2 items-center">
-          <Image
-            src="/facebook.svg"
-            width={40}
-            height={40}
-            alt="Uma imagem simbolizando um robô"
-          />
-        </button>
-        <button
-          className="text-white flex gap-2 items-center"
-          onClick={() =>
-            signIn("github", {
-              redirect: true,
-              callbackUrl: "/",
-            })
-          }
-        >
-          <Image
-            src="/github.svg"
-            width={40}
-            height={40}
-            alt="Uma imagem simbolizando um robô"
-          />
-        </button>
-      </div>
-    </div>
-  );
+ return (
+  <div className='flex h-full w-full flex-col items-center justify-center gap-12'>
+   <h2 className='text-2xl font-medium text-white'>Faça Login</h2>
+   <div className='flex items-center gap-8 rounded-2xl border-[1px] border-gray-950 p-4'>
+    <button className='flex items-center gap-2 text-white'>
+     <Image src='/discord.svg' width={50} height={50} alt='Uma imagem simbolizando um robô' />
+    </button>
+    <button className='flex items-center gap-2 text-white'>
+     <Image src='/google.svg' width={40} height={40} alt='Uma imagem simbolizando um robô' />
+    </button>
+    <button className='flex items-center gap-2 text-white'>
+     <Image src='/facebook.svg' width={40} height={40} alt='Uma imagem simbolizando um robô' />
+    </button>
+    <button
+     className='flex items-center gap-2 text-white'
+     onClick={() =>
+      signIn("github", {
+       redirect: true,
+       callbackUrl: "/",
+      })
+     }>
+     <Image src='/github.svg' width={40} height={40} alt='Uma imagem simbolizando um robô' />
+    </button>
+   </div>
+  </div>
+ );
 }
