9cc149bd299128f8508682ded2176cc49777e146
arisonreis <arisontechp@gmail.com>
2024-08-06 00:46:19 -0300

    manage categories dialog

---
+++ src/app/admin/components/dialogs/manageCategories/index.tsx
@@ -0,0 +1,38 @@
+import {
+ Dialog,
+ DialogContent,
+ DialogDescription,
+ DialogHeader,
+ DialogTitle,
+ DialogTrigger,
+} from "@/components/ui/dialog";
+import { CategoryType } from "@/@types/category";
+import { ProductType } from "@/@types/products";
+import { PackageSearch } from "lucide-react";
+import { DataTable } from "./components/datatable";
+interface IRemoveProductDialog {
+ categories: CategoryType[] | undefined | null;
+ products: ProductType[];
+}
+
+export async function ManageCategoriesDialog({ categories, products }: IRemoveProductDialog) {
+ return (
+  <Dialog>
+   <DialogTrigger className='w-[20rem] rounded-lg bg-slate-900 py-6 text-white'>
+    <span className='flex w-full items-center justify-between px-16'>
+     <p className='text-base'>Gerenciar Categorias</p>
+     <PackageSearch size={18} className='text-color' />
+    </span>
+   </DialogTrigger>
+   <DialogContent className='flex h-[80%] w-full max-w-[90%] flex-col items-center'>
+    <DialogHeader>
+     <DialogTitle>Gerencie categorias</DialogTitle>
+
+     <DialogDescription />
+    </DialogHeader>
+
+    <DataTable data={categories as CategoryType[]} />
+   </DialogContent>
+  </Dialog>
+ );
+}
