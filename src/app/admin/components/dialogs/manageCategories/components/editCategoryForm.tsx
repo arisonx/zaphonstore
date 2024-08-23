b9d72a945f06da6d6cc3c821a3bcfa5281cd9496
arisonx <arisontechp@gmail.com>
2024-08-23 01:13:58 -0300

    removing unused code line

--- src/app/admin/components/dialogs/manageCategories/components/editCategoryForm.tsx
+++ src/app/admin/components/dialogs/manageCategories/components/editCategoryForm.tsx
@@ -14,8 +14,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-import { createCategory, type CreateCategoryResponse } from "@/app/actions/category/createCategory";
 import { useTransition } from "react";
 import { LucideLoader, SquareArrowOutUpRight } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
