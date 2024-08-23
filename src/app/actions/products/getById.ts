17acebae402ccf1e72e23ddf007c19cb5712fd93
arisonx <arisontechp@gmail.com>
2024-08-23 01:10:44 -0300

    updating types when geting a product by id

--- src/app/actions/products/getById.ts
+++ src/app/actions/products/getById.ts
@@ -12,21 +12,23 @@ export async function GetById(id: string): Promise<GetByIdResponse> {
  try {
   const productExists = await fetch("http://localhost:3000/api/product/get-unique", {
    method: "POST",
-   headers: {
-    "Content-Type": "application/json",
+   body: JSON.stringify({ product_id: id }),
+   next: {
+    revalidate: 1000,
    },
-   body: JSON.stringify({ product_i: id }),
   });
 
   if (productExists.status !== 200) {
    return { sucess: false, error: { message: "Product not found" }, product: null };
   }
 
+  const product = await productExists.json();
+
   return {
    error: {
     message: null,
    },
-   product: await productExists.json(),
+   product: product,
    sucess: true,
   };
  } catch (err) {
