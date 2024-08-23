5f89605b7be994e90a8851587607b3000fb4c478
arisonx <arisontechp@gmail.com>
2024-08-23 01:12:52 -0300

    update css and removing dialogs components props

--- src/app/admin/components/dialogs/index.tsx
+++ src/app/admin/components/dialogs/index.tsx
@@ -3,7 +3,7 @@ import { LogsViewDialog } from "./logsview";
 import { CategoryType } from "@/@types/category";
 import { ProductType } from "@/@types/products";
 import { ManageCategoriesDialog } from "./manageCategories";
-
+import { StockDialog } from "./stock";
 interface AdminDialogsData {
  categories: CategoryType[] | undefined | null;
  products: ProductType[] | undefined | null;
@@ -12,11 +12,11 @@ export function AdminDialogs({ categories, products }: AdminDialogsData) {
  return (
   <div className='flex h-[90%] w-full flex-col items-center gap-4'>
    <h2 className='pb-[2rem] pt-[5rem] text-xl text-white'>Zaphon Store Admin</h2>
-   <div className='flex h-[65%] w-full flex-col items-center justify-center gap-2 rounded-md'>
-   
+   <div className='flex h-[65%] w-full flex-col items-center justify-center gap-4 rounded-md'>
     <RemoveProductDialog categories={categories} products={products as ProductType[]} />
-    <ManageCategoriesDialog categories={categories} products={products as ProductType[]} />
+    <ManageCategoriesDialog categories={categories} />
     <LogsViewDialog />
+    <StockDialog categories={categories as CategoryType[]} products={products as ProductType[]} />
    </div>
   </div>
  );
